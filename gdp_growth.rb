5c2682bf716a72dd8540b1b785d03577467f118d
ritvars-lauza <ritvarslauza@gmail.com>
2025-01-20 11:35:02 +0800

    Initial commit of gdp_growth.rb

---
+++ gdp_growth.rb
@@ -0,0 +1,45 @@
+=begin compare country gdp in a chosen year
+=end
+require 'httparty'
+
+year = ARGV[0]
+
+ARGV.clear
+
+puts "Enter the first country for comparison:"
+country_1 = gets.chomp
+
+puts "Enter the second country for the comparison:"
+country_2 = gets.chomp
+
+
+HEADERS = {"X-Api-Key" => "T+qnKw9NM0hAsoV8KVyn7Q==Kv2PQFZ4rGEaHMEx"}
+BASE_URL = 'https://api.api-ninjas.com/v1/gdp?'
+
+
+
+def calculate_gdp_growth(year,country)
+  year_param = "year=" + "#{year}"
+  country_param = "&country="+"#{country}"
+  full_url = BASE_URL + year_param + country_param
+  gdp_growth = HTTParty.get(full_url, headers: HEADERS)[0]["gdp_growth"]
+end
+
+
+gdp_growth_country1  = calculate_gdp_growth(year,country_1)
+gdp_growth_country2 = calculate_gdp_growth(year,country_2)
+
+biggest_country =  country_1
+if calculate_gdp_growth(year,country_1) < calculate_gdp_growth(year,country_2)
+  biggest_country = country_2
+end
+
+puts "In #{year} #{country_1} GDP grew #{gdp_growth_country1} and #{country_2} GDP gew #{gdp_growth_country2}. \n #{biggest_country} grew faster"
+
+
+
+
+
+
+
+
